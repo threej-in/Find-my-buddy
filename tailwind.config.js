75bc47bd08c005935bc9db2aadb0a788561a246b
threej <lekhpal24@gmail.com>
2023-02-18 14:41:15 +0530

    template

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './*.php',
+    './**/*.php'
+  ],
   theme: {
     extend: {},
   },
---
+++ template/header.php
@@ -0,0 +1,4 @@
+<header class="flex justify-between w-full bg-slate-700 text-white p-2 px-3">
+    <h1 class=" font-bold">Find my buddy</h1>
+    <span class=" top-0 rotate-90 float-right">|||</span>
+</header>
