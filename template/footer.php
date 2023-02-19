5e82f12f9624e1c5d37ff9b381e39abaf8ff6b8b
threej <lekhpal24@gmail.com>
2023-02-19 15:20:01 +0530

    navigation links & footer added

--- template/footer.php
+++ template/footer.php
@@ -1,1 +1,7 @@
 <?php print_r($_extra_script_tags ?? '') ?>
+<footer class="bg-slate-700 p-5">
+    <div class="flex flex-row text-cyan-100 gap-2 w-1/4">
+        <img class="w-16 rounded" src="assets/img/logo.jpeg" alt="">
+        <h3>We help you find your activities partner</h3>
+    </div>
+</footer>
--- template/header.php
+++ template/header.php
@@ -1,4 +1,8 @@
 <header class="flex justify-between w-full bg-slate-700 text-white p-2 px-3">
-    <h1 class=" font-bold">Find my buddy</h1>
-    <span class=" top-0 rotate-90 float-right">|||</span>
+    <h1 class=" font-bold cursor-pointer" onClick="location.href='.'">Find my buddy</h1>
+    <span class=" top-0 float-right">
+        <ul class=" cursor-pointer">
+            <li onClick="location.href='page/login.php'"> Login</li>
+        </ul>
+    </span>
 </header>
